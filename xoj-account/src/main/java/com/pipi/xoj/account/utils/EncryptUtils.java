8320ad6654414611199aaaf922b470bd2cc57ee6
guo_x <guo_x0315@163.com>
2024-03-28 20:18:54 +0800

    Now unified gateway configuration is supported

---
+++ xoj-account/src/main/java/com/pipi/xoj/account/utils/EncryptUtils.java
@@ -0,0 +1,11 @@
+/**
+ * @Time: 2024/3/27 21:33
+ * @Author: guoxun
+ * @File: EncryptUtils
+ * @Description: 用户密码加密解密工具
+ */
+
+package com.pipi.xoj.account.utils;
+
+public class EncryptUtils {
+}
